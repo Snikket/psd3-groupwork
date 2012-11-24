a4daa84ff73357a5fc4975b9d6cc7d591c3163ec
Andrew Gardner <0405116g@student.gla.ac.uk>
2012-11-24 18:34:46 +0000

    Cleaned up login.sh

--- Deliverables/prototype/login.sh
+++ Deliverables/prototype/login.sh
@@ -1,8 +1,16 @@
-clear 
-echo "------------------------------------------------------"
-echo "-- Welcome to the DCS Internship Management System! --"
-echo "------------------------------------------------------"
-echo
+# function to clear screen and display header
+function displayHeader {
+	clear
+	echo "------------------------------------------------------"
+	echo "-- Welcome to the DCS Internship Management System! --"
+	echo "------------------------------------------------------"
+	echo
+}
+
+# display header
+displayHeader
+
+# login and password
 echo -n "Login: " # -n allows input at the end of this line
 read username
 echo -n "Password: "
@@ -11,26 +19,34 @@ read password
 stty echo
 echo
 echo
+
+# check and process input
 if [ $username == "tws3r" ]; then
+	displayHeader
 	echo "Logging in..."
 	echo
-	export USERTYPE="coordinator"
 	sleep 2
 	#bash ./adminHome.sh
 elif [ $username == "1005730w" ]; then
+	displayHeader
 	echo "Logging in..."
 	echo	
-	export USERTYPE="student"
 	sleep 2
 	#bash ./studentHome.sh
 elif [ $username == "IBM" ]; then
+	displayHeader
 	echo "Logging in..."
 	echo
-	export USERTYPE="company"
 	sleep 2
 	#bash ./companyHome.sh
 else
-	echo "Incorrect username or password, please try again."
+	displayHeader
+	echo "Logging in..."
+	echo
+	sleep 2
+	displayHeader
+	echo "Incorrect username or password."
+	echo "Please try again."
 	echo
 	sleep 2
 	bash ./login.sh
